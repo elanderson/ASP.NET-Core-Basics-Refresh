8383787bcaf4801979df3b4b4d07a7f5fcefb2cb
Eric Anderson <eanderson@efcsystems.com>
2019-10-04 06:30:25 -0500

    Add contacts controller

---
+++ src/ContactsApi/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace ContactsApi.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
