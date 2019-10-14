14d130df7102c0603d532a2aef4d459292359b9d
Eric Anderson <elanderson@gmail.com>
2019-10-14 06:51:59 -0500

    Data access (#2)

--- src/ContactsApi/Controllers/ContactsController.cs
+++ src/ContactsApi/Controllers/ContactsController.cs
@@ -1,36 +1,108 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using ContactsApi.Models;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ContactsApi.Data;
+using ContactsApi.Models;
 
 namespace ContactsApi.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class ContactsController : ControllerBase
     {
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ContactsDbContext _context;
 
-        public ContactsController(ILogger<WeatherForecastController> logger)
+        public ContactsController(ContactsDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        // GET: api/Contacts
         [HttpGet]
-        public IEnumerable<Contact> Get()
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
         {
-            return Enumerable.Range(1, 5).Select(index => new Contact
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
             {
-                Id = index,
-                Name = $"Test{index}",
-                Address = $"{index} Main St.",
-                City = "Nashville",
-                State = "TN",
-                PostalCode = "37219",
-                Phone = "615-555-5555",
-                Email = $"test{index}@test.com"
-            });
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        // PUT: api/Contacts/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact(int id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contacts
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContact", new { id = contact.Id }, contact);
+        }
+
+        // DELETE: api/Contacts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Contact>> DeleteContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return contact;
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
         }
     }
 }
