16caa14e3d1495ef86a69ad71fd9618d330f486c
Eric Anderson <elanderson@gmail.com>
2019-12-03 05:58:01 -0600

    Add request response logging

---
+++ src/ContactsApi/Middleware/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ContactsApi.Middleware
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestResponseLoggingMiddleware>();
+        }
+    
+        public async Task Invoke(HttpContext context)
+        {  
+            _logger.LogInformation(await FormatRequest(context.Request));
+
+            var originalBodyStream = context.Response.Body;
+
+            using (var responseBody = new MemoryStream())
+            {
+                context.Response.Body = responseBody;
+
+                await _next(context);
+
+                _logger.LogInformation(await FormatResponse(context.Response));
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+    
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+            request.EnableBuffering();
+
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            request.Body.Position = 0;
+
+            request.Body = body;
+
+            return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(response.Body).ReadToEndAsync(); 
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return $"Response {text}";
+        }
+    }
+}
