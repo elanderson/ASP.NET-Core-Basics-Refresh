2eedb30cbe78a1fc1c87a6227e2b6fd880ac8656
Eric Anderson <elanderson@gmail.com>
2019-12-10 05:43:51 -0600

    Rework logging

--- src/ContactsApi/Middleware/RequestResponseLoggingMiddleware.cs
+++ src/ContactsApi/Middleware/RequestResponseLoggingMiddleware.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.IO;
 
 namespace ContactsApi.Middleware
 {
@@ -11,49 +11,78 @@ namespace ContactsApi.Middleware
     {
         private readonly RequestDelegate _next;
         private readonly ILogger _logger;
+        private readonly RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
 
         public RequestResponseLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
             _logger = loggerFactory.CreateLogger<RequestResponseLoggingMiddleware>();
+            _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
         }
-    
-        public async Task Invoke(HttpContext context)
-        {  
-            _logger.LogInformation(await FormatRequest(context.Request));
 
+        public async Task Invoke(HttpContext context)
+        {
+            await LogRequest(context);
+            await LogResponse(context);
+        }
+
+        private async Task LogRequest(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+
+            await using var requestStream = _recyclableMemoryStreamManager.GetStream();
+            await context.Request.Body.CopyToAsync(requestStream);
+            _logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +
+                                   $"Host: {context.Request.Host} " +
+                                   $"Path: {context.Request.Path} " +
+                                   $"QueryString: {context.Request.QueryString} " +
+                                   $"Request Body: {ReadStreamInChunks(requestStream)}");
+            context.Request.Body.Position = 0;
+        }
+
+        private async Task LogResponse(HttpContext context)
+        {
             var originalBodyStream = context.Response.Body;
 
-            using (var responseBody = new MemoryStream())
+            await using var responseBody = _recyclableMemoryStreamManager.GetStream();
+            context.Response.Body = responseBody;
+
+            await _next(context);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+            _logger.LogInformation($"Http Response Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +
+                                   $"Host: {context.Request.Host} " +
+                                   $"Path: {context.Request.Path} " +
+                                   $"QueryString: {context.Request.QueryString} " +
+                                   $"Response Body: {text}");
+
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+
+        private static string ReadStreamInChunks(Stream stream)
+        {
+            const int readChunkBufferLength = 4096;
+            
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using var textWriter = new StringWriter();
+            using var reader = new StreamReader(stream);
+
+            var readChunk = new char[readChunkBufferLength];
+            int readChunkLength;
+
+            do
             {
-                context.Response.Body = responseBody;
+                readChunkLength = reader.ReadBlock(readChunk, 0, readChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            } while (readChunkLength > 0);
 
-                await _next(context);
-
-                _logger.LogInformation(await FormatResponse(context.Response));
-                await responseBody.CopyToAsync(originalBodyStream);
-            }
-        }
-    
-        private async Task<string> FormatRequest(HttpRequest request)
-        {
-            request.EnableBuffering();
-
-            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
-            await request.Body.ReadAsync(buffer, 0, buffer.Length);
-
-            request.Body.Position = 0;
-
-            return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {Encoding.UTF8.GetString(buffer)}";
-        }
-
-        private async Task<string> FormatResponse(HttpResponse response)
-        {
-            response.Body.Seek(0, SeekOrigin.Begin);
-            var text = await new StreamReader(response.Body).ReadToEndAsync(); 
-            response.Body.Seek(0, SeekOrigin.Begin);
-
-            return $"Response {text}";
+            return textWriter.ToString();
         }
     }
 }
