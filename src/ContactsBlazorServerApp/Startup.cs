27fc723c79672432fa9c3fc8c9173c87c63accb9
Eric Anderson <elanderson@gmail.com>
2019-12-17 06:17:05 -0600

    Add contact list

--- src/ContactsBlazorServerApp/Startup.cs
+++ src/ContactsBlazorServerApp/Startup.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Apis;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,9 +22,11 @@ namespace ContactsBlazorServerApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages()
+                    .AddNewtonsoftJson();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient<IContactsClient, ContactsClient>(client => client.BaseAddress = new Uri(Configuration.GetSection("ContactsApi").Value));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
