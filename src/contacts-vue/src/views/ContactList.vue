a7e807f6fb4195caafb357c4b037d01cf41a33aa
Eric Anderson <elanderson@gmail.com>
2020-01-03 06:29:20 -0600

    Add Vue Project (#10)

---
+++ src/contacts-vue/src/views/ContactList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ContactListComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContactListComponent from '@/components/ContacList'
+
+export default {
+  name: 'contactList',
+  components: {
+    ContactListComponent
+  }
+}
+</script>
